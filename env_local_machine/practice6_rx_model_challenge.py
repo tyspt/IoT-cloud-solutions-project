aa170826f91bba99cfdeb8108786762bd53a214c
Jiawei Tang <jiaweitang828@gmail.com>
2019-10-22 02:01:22 +0200

    practice 6 rx model

---
+++ env_local_machine/practice6_rx_model_challenge.py
@@ -0,0 +1,44 @@
+import threading
+import random
+from time import sleep
+from rx.subject import Subject
+from rx import operators as ops
+
+mysubject = Subject()
+
+
+def callback(r):
+    print(r)
+    mysubject.on_next(r)
+    return r
+
+
+def incoming():
+    for i in range(0, 1000):
+        value = random.randint(0, 10)
+        delay = random.randint(0, 3)
+        sleep(float(delay))
+        callback(value)
+
+
+x = threading.Thread(target=incoming)
+x.start()
+
+searcher = mysubject.pipe(
+    ops.buffer_with_time(3)
+    #ops.map(lambda x: x*2), \
+    #ops.filter(lambda i : i > 10)
+)
+
+# called every buffer interval
+def send_response(x):
+    average = sum(x) / len(x)
+    print("send response", x)
+    print(average)
+
+
+def on_error(ex):
+    print("E", ex)
+
+
+searcher.subscribe(send_response, on_error)
