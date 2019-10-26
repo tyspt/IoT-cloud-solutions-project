498bffc0a032d1d76c082b157f1fd7500d0c0edf
Jiawei Tang <jiaweitang828@gmail.com>
2019-10-26 03:53:28 +0200

    changed folder structure

--- env_rasp4/global_config.py
+++ env_rasp4/global_config.py
@@ -1,4 +1,4 @@
 
 # Time between two pictures are taken in seconds. Pi needs ca. 3 Seconds to take one picture 
 # so this value should be larger than 3
-CAMERA_INTERVAL_SECONDS = 5
+CAMERA_INTERVAL_SECONDS = 3600
