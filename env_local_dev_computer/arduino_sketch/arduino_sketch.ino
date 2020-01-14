2f78ff94b140e230ec6609fd789b34af90de0afa
Jiawei Tang <jiaweitang828@gmail.com>
2020-01-14 11:08:55 +0100

    add tds measurement to arduino

--- env_local_dev_computer/arduino_sketch/arduino_sketch.ino
+++ env_local_dev_computer/arduino_sketch/arduino_sketch.ino
@@ -16,6 +16,7 @@ static const int WATER_LEVEVL_SENSOR_ANALOG_PIN = A0;
 static const int LIGHT_SENSOR_ANALOG_PIN = A2;
 static const int PH_SENSOR_ANALOG_PIN = A3;
 static const int TURBIDITY_SENSOR_ANALOG_PIN = A4;
+static const int TDS_SENSOR_ANALOG_PIN = A5;
 
 //create json objects for storing data
 const size_t capacity = JSON_OBJECT_SIZE(4);
@@ -26,6 +27,7 @@ DynamicJsonDocument brightness_json(capacity);
 DynamicJsonDocument distance_json(capacity);
 DynamicJsonDocument ph_json(capacity);
 DynamicJsonDocument turbidity_json(capacity);
+DynamicJsonDocument tds_json(capacity);
 
 DHT_nonblocking dht_sensor( DHT_SENSOR_DIGITAL_PIN, DHT_SENSOR_TYPE );
 SR04 sr04_ultrasonic = SR04(DISTANCE_SENSOR_DIGITAL_PIN_ECHO,DISTANCE_SENSOR_DIGITAL_PIN_TRIG); //object library for ultrasonic sensor
@@ -72,6 +74,11 @@ void setup( )
   turbidity_json["sensor"] = "turbidity";
   turbidity_json["data"] = -1.0f;
   turbidity_json["unit"] = "percent";
+
+  tds_json["timestamp"] = 1ul;
+  tds_json["sensor"] = "tds";
+  tds_json["data"] = -1.0f;
+  tds_json["unit"] = "percent";
   
   Serial.begin(9600);
 }
@@ -215,6 +222,29 @@ static bool measure_water_turbidity(float *turbidity){
   return ( false );
 }
 
+/* Water tds (total dissolved solids) measurement
+ *  
+ * Poll for a measurement, keeping the state machine alive.  Returns
+ * true if a measurement is available.
+ */
+static bool measure_water_tds(float *tds){
+  static unsigned long measurement_timestamp = millis( );
+  
+  /* Measure once every inverval */
+  if( millis( ) - measurement_timestamp > (READING_INTERVAL_SECONDS * 1000ul) || millis( ) < measurement_timestamp )
+  {   
+      //convert analog value to tds
+      int measure = analogRead(TDS_SENSOR_ANALOG_PIN);
+      double voltage = 5 / 1024.0 * measure; //classic digital to voltage conversion
+      *tds = voltage / 5.0 * 100.0;
+      
+      measurement_timestamp = millis( );
+      return( true );
+  }
+  
+  return ( false );
+}
+
 
 /*
  * Main program loop.
@@ -226,6 +256,7 @@ void loop( )
   float water_level;
   float ph;
   float turbidity;
+  float tds;
   
   int brightness; 
   int distance;
@@ -300,6 +331,17 @@ void loop( )
     turbidity_json["data"] = turbidity;
     
     serializeJson(turbidity_json, Serial);
+    Serial.println();
+  }
+
+  /* Measure water tds (total dissolved solids).  If the functions returns
+   true, then a measurement is available. */
+  if(measure_water_tds(&tds) == true)
+  {
+    tds_json["timestamp"] = millis();
+    tds_json["data"] = tds;
+    
+    serializeJson(tds_json, Serial);
     Serial.println();
   }
 }
